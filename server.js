6579e0df931be2ce25cd2337db8d82a8aef0ad4a
fk156664 <156646831+fk156664@users.noreply.github.com>
2024-04-23 08:58:32 +0000

    ...

--- server.js
+++ server.js
@@ -8,14 +8,30 @@
 const express = require('express');
 
 // Constants
+//Die Anweisungen werden von oben nach unten abgearbeitet. 
+//Der Wert 3000 wird von rechts nach links zugewiesen an die Konstante namens PORT. 
+//Das einfache Gkleichheitszeichen lässt sich mit "... wird zugewiesen an..."
 const PORT = 3000;
+//Der Wert '0.0.0.0' wird zugewiesen an eine Konstante namen HOST.
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
+
+	//res ist die Antwort des Serves an den Browser.
+	//send() ist die Anweisung etwas an den Browser zu senden.
+	//&'hello world..' ist der Wert, der an die Anweisung send() übergeben wird.
 	res.send('Hello remote world!\n');
 });
 
-app.listen(PORT, HOST);
+// Mit listen() wird der Server angewiesen, auf den angegebenen HOST und PORT
+//zu lauschen.
+app. listen(PORT, HOST);
+
+//Mit der Anweisung console.log() wird dem Server-Administerator auf der 
+//Konsole angezeigt, was der Server macht. Der Programmierer schreibt 
+//dazu in die runden Klammern den Ausdruck, der auf der Konsole angezeigt werden soll.
+//Die Werte der beiden Konstanten HOST und pOrt werden in den Ausdruck übergeben.
+//Ein Verb mit anschliessenden runden Klammern steht immer für eine Anweisung etwas zu tun.
 console.log(`Running on http://${HOST}:${PORT}`);
