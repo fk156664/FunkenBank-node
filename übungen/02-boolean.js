ece2e091122bd9d71f0dbce6bec9e536275f587e
fk156664 <156646831+fk156664@users.noreply.github.com>
2024-09-16 07:08:04 +0000

    vjfguzjgf

--- übungen/02-boolean.js
+++ übungen/02-boolean.js
@@ -13,13 +13,27 @@ if(true){
     console.log("Der Ausdruck ist unwahr / falsch / false")
 }
 
+// Weil in dieser Aufgabe die Prüfung immer wahr ist if(true), wird immer die Anweisung hinter if ausgeführt.
+// Die Anweisung hinter else kommt niemals zur Ausführung.
+
 console.log("Aufgabe 02")
 
 // In erweiterung von Aufgabe 1 soll der Ausdruck dynamisch entweder true oder false sein
 
 let zahl = 4
+
+// Hier wird mit einem Prüfoperator auf wahr oder flasch geprüft.
+
+// Prüfoperatoren:
+// ==   : Prüfung auf Gleichheit im Wert: if(1 == '1') true
+// ===  : Prüfung auf Gleichheit im Wert und im Typ: if(1 === '1') false
+// >    : grösser als
+// <    : kleiner als
+// >=   : grösser oder gleich
+// <=   : kleiner oder gleich
+
 if(zahl>5){
-    console.log("das ist wah.r")
+    console.log("das ist wahr")
 }else{
     console.log("das ist falsch.")
 }
@@ -81,4 +95,55 @@ if(fuehrerschein){   // führerschein ist schon true
     }
 }else{
     console.log("fahrradfahren")
+}
+
+
+console.log('Aufgabe 04')
+// Zahlen können in Javascript true oder false sein.
+
+let Zahl = 0
+
+if(Zahl){
+    console.log("Die Zahl " + Zahl + " nimmt den boolschen Wert true an ")
+}else{
+    console.log("Die Zahl " + Zahl + " nimmt den boolschen Wert false an ")
+}
+
+// Positive & negative Zahlen sind true.
+// Die Ziffer 0 ist false.
+
+
+console.log("Aufgabe 05")
+
+// Auf einem Sparkonte soll bei einem positiven Kontostand auf der console stehen: "Zinsen berechnen", 
+// wenn kein Guthaben existiert: "Keine Zinsen"
+
+
+let kontostand = 0
+
+if(kontostand){  // auf den prüfoperator kann verzichtet werden, da Zahlen bereits true oder false sind.
+    console.log("Zinsen berechnen für Kontostand " + kontostand)
+}else{
+    console.log("keine Zinsen, da Kontostand 0")
+}
+
+
+console.log("Aufgabe 06")
+
+// In der Bundeslieger gibt es punkte für Spiele
+// Sieg: 3 Punkte
+// Unentschieden: 1 Punkt
+// Niederlage: 0 Punkte
+
+let spielausgang = "Sieg"
+let mannschaft = "deutschland"
+
+if(spielausgang == "Sieg"){
+    console.log("Es gibt 3 Punkte")
+}
+if(spielausgang == "Sieg"){
+    console.log("Es gibt 1 Punkt")
+}
+if(spielausgang == "Sieg"){
+    console.log("Es gibt 0 Punkte")
 }
